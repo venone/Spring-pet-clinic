d3facd7684793e527302e023e34316f4fe433f23
Pritesh <growupprit@gmail.com>
2022-05-29 19:15:56 +0530

    Initial setup

---
+++ src/main/java/pritesh/spring/springpet/SpringPetApplication.java
@@ -0,0 +1,13 @@
+package pritesh.spring.springpet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringPetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pritesh/spring/springpet/SpringPetApplicationTests.java
@@ -0,0 +1,13 @@
+package pritesh.spring.springpet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
